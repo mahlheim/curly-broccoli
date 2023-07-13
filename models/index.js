2b355c5014ec42adddca99e4038cc982c8840d91
kingpin <monique.ahlheim@live.com>
2023-07-13 14:06:30 -0400

    added models

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const Location = require('./Location');
+const Cat = require('./Cat');
+const Dog = require('./Dog');
+
+Location.hasMany(Cat, {
+  foreignKey: 'location_id',
+  onDelete: 'CASCADE',
+});
+
+Location.hasMany(Dog, {
+    foreignKey: 'location_id',
+    onDelete: 'CASCADE',
+  });
+
+Cat.belongsTo(Location, {
+  foreignKey: 'location_id',
+});
+
+Dog.belongsTo(Location, {
+  foreignKey: 'location_id',
+});
+
+module.exports = { Location, Cat, Dog };
