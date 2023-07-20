ce822c66e1556a181e00d9617712a2f2c9126dc6
kingpin <monique.ahlheim@live.com>
2023-07-20 11:51:30 -0400

    fixed id issue in get requests, began work on profile get request

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,30 +1,45 @@
-// Function to fetch and display the user profile
-const fetchUserProfile = async () => {
-  try {
-    const response = await fetch('/api/users/profile');
-    if (response.ok) {
-      const profile = await response.json();
-      displayUserProfile(profile);
-    } else {
-      throw new Error('Failed to fetch user profile');
-    }
-  } catch (error) {
-    console.error(error);
-    // Handle error case, e.g., display an error message
-  }
-};
+// // Function to fetch and display the user profile
+// const fetchUserProfile = async () => {
+//   try {
+//     const response = await fetch('/profile/:id');
+//     if (response.ok) {
+//       const profile = await response.json();
+//       displayUserProfile(profile);
+//     } else {
+//       throw new Error('Failed to fetch user profile');
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     // Handle error case, e.g., display an error message
+//   }
+// };
 
-// Function to display the user profile data on the page
-const displayUserProfile = (profile) => {
-  const usernameElement = document.querySelector('.username');
-  const emailElement = document.querySelector('.email');
-  const passwordElement = document.querySelector('.password');
+// // Function to display the user profile data on the page
+// const displayUserProfile = (profile) => {
+//   const usernameElement = document.querySelector('.username');
+//   const emailElement = document.querySelector('.email');
+//   const passwordElement = document.querySelector('.password');
 
-  usernameElement.textContent = profile.username;
-  emailElement.textContent = profile.email;
-  passwordElement.textContent = profile.password;
-};
+//   usernameElement.textContent = profile.username;
+//   emailElement.textContent = profile.email;
+//   passwordElement.textContent = profile.password;
+// };
 
-// Call fetchUserProfile when the page loads
-window.addEventListener('DOMContentLoaded', fetchUserProfile);
+// // Call fetchUserProfile when the page loads
+// // window.addEventListener('DOMContentLoaded', fetchUserProfile);
+
+// const profile = async () => {
+//   const response = await fetch('/user/:id', {
+//     method: 'GET',
+//     headers: { 'Content-Type': 'application/json' },
+//   });
+
+//   if (response.ok) {
+//     document.location.replace('profile');
+//   } else {
+//     alert('Logout failed. Please try again.');
+//   }
+// };
+
+// document.querySelector('#profile').addEventListener('click', profile);
   
